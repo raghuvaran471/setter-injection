a3997c9a59c7319166f4ab1cfdd06e08bcc7da05
raghuvaran471 <raghuvaran471@gmail.com>
2020-04-27 19:51:10 +0530

    first

---
+++ src/beans/Engine.java
@@ -0,0 +1,19 @@
+package beans;
+
+public class Engine {
+	private String modelyear;
+	
+	public Engine(){
+		System.out.println("Engine");
+	}
+
+	public String getModelyear() {
+		return modelyear;
+	}
+
+	public void setModelyear(String modelyear) {
+		this.modelyear = modelyear;
+	}
+	
+
+}
